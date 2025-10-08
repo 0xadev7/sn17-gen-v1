72c49aa0914dd09850af5cb9cd6e0b24a3fdf1f3
0xadev7 <leric7510@gmail.com>
2025-10-08 12:08:46 -0400

    fix: flux load

--- gen/pipelines/t2i_flux.py
+++ gen/pipelines/t2i_flux.py
@@ -1,24 +1,14 @@
 from __future__ import annotations
 import torch
 from diffusers import FluxPipeline
-from transformers import T5EncoderModel, T5Tokenizer  
 from PIL import Image
 
 
 class FluxText2Image:
     def __init__(self, device: torch.device):
-        tokenizer = T5Tokenizer.from_pretrained("t5-base")
-
-        text_encoder = T5EncoderModel.from_pretrained(
-            "t5-base",
-        )
-
         self.pipe = FluxPipeline.from_pretrained(
             "black-forest-labs/FLUX.1-schnell",
             torch_dtype=torch.bfloat16,
-            # device_map="balanced",
-            text_encoder=text_encoder,
-            tokenizer=tokenizer,
         )
         self.pipe.to(device)
         self.device = device
