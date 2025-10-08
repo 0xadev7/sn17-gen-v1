16beb1d8bac2bd234ecf67598dfd21504f6e8d4d
0xadev7 <leric7510@gmail.com>
2025-10-08 10:16:07 -0400

    fix: install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ conda env create -f env/environment-cu124.yml
 conda activate sn17-miner-v1
 
 # Trellis
-cd trellis
+cd scripts/trellis
 bash setup.sh --basic --xformers --flash-attn --diffoctreerast --spconv --mipgaussian --kaolin --nvdiffrast
