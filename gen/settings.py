b5b9dd820d79887863fbb3fc5c369d843f490bb0
0xadev7 <leric7510@gmail.com>
2025-10-09 11:56:42 -0400

    fix: initial params

--- gen/settings.py
+++ gen/settings.py
@@ -26,15 +26,13 @@ class Config:
 
     # Text-to-2D parameters
     t2i_steps: int = int(os.getenv("T2I_STEPS", 2))
-    t2i_guidance: float = float(os.getenv("T2I_GUIDANCE", 3.5))
-    t2i_res: int = int(os.getenv("T2I_RES", 704))
-    t2i_max_tries: int = int(os.getenv("T2I_MAX_TRIES", 3))
+    t2i_guidance: float = float(os.getenv("T2I_GUIDANCE", 4.0))
+    t2i_res: int = int(os.getenv("T2I_RES", 1024))
+    t2i_max_tries: int = int(os.getenv("T2I_MAX_TRIES", 1))
 
     # Trellis parameters
-    trellis_struct_steps: int = int(os.getenv("TRELLIS_STRUCT_STEPS", 6))
-    trellis_slat_steps: int = int(os.getenv("TRELLIS_SLAT_STEPS", 6))
+    trellis_struct_steps: int = int(os.getenv("TRELLIS_STRUCT_STEPS", 12))
+    trellis_slat_steps: int = int(os.getenv("TRELLIS_SLAT_STEPS", 12))
     trellis_cfg_struct: float = float(os.getenv("TRELLIS_CFG_STRUCT", 7.5))
     trellis_cfg_slat: float = float(os.getenv("TRELLIS_CFG_SLAT", 3.0))
-    trellis_max_gaussians: int = int(os.getenv("TRELLIS_MAX_GS", 600_000))
-    trellis_target_mb: int = int(os.getenv("TRELLIS_TARGET_MB", 40))
-    trellis_max_tries: int = int(os.getenv("TRELLIS_MAX_TRIES", 2))
+    trellis_max_tries: int = int(os.getenv("TRELLIS_MAX_TRIES", 3))
